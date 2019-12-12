39197d9cc8ae2a1d0d6982735f1981b4b1e40f47
Edward Heaver <heaver.edward02@gmail.com>
2019-12-12 14:04:41 +0000

    Done.

--- app.rb
+++ app.rb
@@ -3,6 +3,17 @@ class App < Sinatra::Base
 	get '/' do
 		erb :index
 	end
-
-
+	
+"	get "/hello"
+	  erb :hello
+	end
+	
+	get "/goodbye"
+	  erb :goodbye
+	end
+	
+	get "/date"
+	  erb :date
+	end"
+	
 end
